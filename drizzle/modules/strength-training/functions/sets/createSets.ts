d16528d77ef7c9ce6ce6cdc3449249c065e6262a
David L. Bowman <david@theinnovationlab.dev>
2025-01-02 10:30:10 -0600

    moved some files

--- drizzle/modules/strength-training/functions/sets/createSets.ts
+++ drizzle/modules/strength-training/functions/sets/createSets.ts
@@ -1,5 +1,6 @@
 "use server";
 
+import { PRIMARY_LIFT_PATTERNS } from "@/drizzle/modules/strength-training/constants/liftPatterns";
 import {
 	type ExerciseDefinitionsSelect,
 	ExerciseType,
@@ -17,42 +18,6 @@ interface SetScheme {
 	percentageOfMax?: number;
 	rpe?: number;
 }
-
-// Week patterns for primary lifts
-const PRIMARY_LIFT_PATTERNS = {
-	week1: [
-		{ reps: 10, percentageOfMax: 45 },
-		{ reps: 5, percentageOfMax: 55 },
-		{ reps: 5, percentageOfMax: 65 },
-		{ reps: 5, percentageOfMax: 75 },
-		{ reps: 5, percentageOfMax: 85 },
-		{ reps: 5, percentageOfMax: 85 },
-	],
-	week2: [
-		{ reps: 8, percentageOfMax: 50 },
-		{ reps: 3, percentageOfMax: 60 },
-		{ reps: 3, percentageOfMax: 70 },
-		{ reps: 3, percentageOfMax: 80 },
-		{ reps: 3, percentageOfMax: 90 },
-		{ reps: 3, percentageOfMax: 90 },
-	],
-	week3: [
-		{ reps: 10, percentageOfMax: 50 },
-		{ reps: 5, percentageOfMax: 70 },
-		{ reps: 3, percentageOfMax: 80 },
-		{ reps: 2, percentageOfMax: 85 },
-		{ reps: 1, percentageOfMax: 90 },
-		{ reps: 1, percentageOfMax: 95 },
-	],
-	week4: [
-		{ reps: 10, percentageOfMax: 50 },
-		{ reps: 8, percentageOfMax: 60 },
-		{ reps: 5, percentageOfMax: 70 },
-		{ reps: 5, percentageOfMax: 70 },
-		{ reps: 5, percentageOfMax: 70 },
-		{ reps: 5, percentageOfMax: 70 },
-	],
-};
 
 const NON_PRIMARY_SETS = 6;
 
